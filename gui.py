2d4085c4b4be989b344b1c55d6a81002ebd55eab
Mr. Simonsen <johnsimonsen@weber.edu>
2019-08-18 12:45:09 -0600

    added second example and better tutorial resource to gui

--- gui.py
+++ gui.py
@@ -1,13 +1,30 @@
-#https://docs.python.org/3/library/tkinter.html
-import tkinter as tk
+#https://tkdocs.com/tutorial/
+from  tkinter import *
+from tkinter import ttk
+
+class Application (Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.alld = ''
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.all_day = Checkbutton(self)
+        self.all_day['text']='All day event'
+        self.all_day['variable']=self.alld
+        self.all_day['onvalue']='True'
+        self.all_day['offvalue']='False'
+        self.all_day['command']=self.test(self.alld)
+        self.all_day.pack(side='top')
+
+    def test(self,name):
+        print(f'{name} clicked')
 
 
 
-
-
-
-
-
-root = tk.Tk()
+root = Tk()
+root.title("AB Day Calendar Creator")
 app = Application(master=root)
 app.mainloop()
